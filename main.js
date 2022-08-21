25930462ada63fe05a6edb71b0e6c2b207963ca5
VaishnavArhantMovva <75323818+VaishnavArhantMovva@users.noreply.github.com>
2022-08-21 13:36:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function setup()
+{
+    canvas = createCanvas(500,400);
+    canvas.position(900,200);
+    video = createCapture(VIDEO);
+    video.size(500,400);
+    video.position(200,200);
+}
+
